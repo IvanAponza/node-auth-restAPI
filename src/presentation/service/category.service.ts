c3d42b8fb154b0a2c26f69fdd76bdb5daf72a392
ivan <aponzacantoni@gmail.com>
2024-10-20 20:47:17 -0500

    feature: added pagination and implement in the category

--- src/presentation/service/category.service.ts
+++ src/presentation/service/category.service.ts
@@ -1,5 +1,5 @@
 import { CategoryModel } from "../../data";
-import { CreateCategoryDto, CustomError, UserEntity } from "../../domain";
+import { CreateCategoryDto, CustomError, PaginationDto, UserEntity } from "../../domain";
 
 
 
@@ -30,20 +30,51 @@ export class CategoryService{
         }
     }
     
-    async getCategories(){
+    async getCategories(paginationDto: PaginationDto){
+
+        const { page, limit } = paginationDto;
 
         try {
             
-            const categories = await CategoryModel.find()
-    
-            return categories.map( category => ({
-                id: category.id,
-                name: category.name,
-                available: category.available,
-            }));
-    
-            // Forma Implicita
-            // return categories.map(({id, name, available}) => ({id, name, available}));
+            // const total = await CategoryModel.countDocuments();
+            // const categories = await CategoryModel.find()
+            //     .skip( (page - 1) * limit )//salta una cantidad de regist (0 - limit)
+            //     .limit( limit )//trae cant reg deseado
+
+            const [ total, categories ] = await Promise.all([
+                CategoryModel.countDocuments(),
+                CategoryModel.find()
+                    .skip( (page - 1) * limit )
+                    .limit( limit )
+            ]);
+
+                
+                // return categories.map( category => ({
+                //     id: category.id,
+                //     name: category.name,
+                //     available: category.available,
+                // }));
+                    
+                // Forma Implicita
+                // return categories.map(({id, name, available}) => ({id,name, available}));
+
+                //Colocamos la res en un ojeto
+                return {
+                    page: page,
+                    limit: limit,
+                    total: total,
+                    next: `/api/categories?page=${( page + 1)}&limit=${ limit}`,
+                    prev: (page - 1 > 0 ) ? `/api/categories?page=${( page - 1)}&limit=${ limit}`: null,
+
+                    // categories: categories.map( category => ({
+                    //     id: category.id,
+                    //     name: category.name,
+                    //     available: category.available,
+                    // }))  
+                    
+                    categories: categories.map(({id, name, available}) => ({id,name, available}))
+                }
+                
         } catch (error) {
             throw CustomError.InternalServer('Internal Server Error')
         }
