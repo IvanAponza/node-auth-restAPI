27ae0a7da28198901a00ca0605bb482885b162ab
ivan <aponzacantoni@gmail.com>
2025-03-13 21:25:05 -0500

    fix: se agrega validacion mongoDB ID

--- src/config/validations.ts
+++ src/config/validations.ts
@@ -1,19 +1,14 @@
 import mongoose from "mongoose"
 
-
 export const validations = {
 
   // Expresión regular para validar correos electrónicos
   email: /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/,
   
-  
 }
 
 export class Validators {
-
   static isMongoID( id: string ){
     return mongoose.isValidObjectId(id);
   }
-  
-  
 }
